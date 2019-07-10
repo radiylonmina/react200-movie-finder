d622a5959b1a99a781922488d41c6ae14cf7f552
radiylonmina <radiylon@gmail.com>
2019-07-09 17:13:24 -0700

    setup initial containers, verify basic views

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import MovieSearchContainer from './containers/MovieSearchContainer';
+import MovieDetailContainer from './containers/MovieDetailContainer';
 
-export default class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
-    );
+      <Router>
+        <div className='container'> 
+          <Route exact path='/' component={ MovieSearchContainer } />
+          <Route path='/movie/:id' component={ MovieDetailContainer } />
+        </div>
+      </Router>
+    )
   }
 }
+
+export default App;
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promise from 'redux-promise-middleware';
+// import reducers from './reducers';
 import App from './app';
 
-/* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-/* eslint-enable */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const store = createStore(composeEnhancers(applyMiddleware(promise)));
 
+/* Update render method invoke */
 render(
   <Provider store={ store }>
     <App />
